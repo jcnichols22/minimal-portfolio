f7d5c42bef9851efcc48bb0a34a2559d7873ac13
Josh Nichols <jcnichols22@gmail.com>
2022-12-23 09:02:34 -0500

    first commit

---
+++ src/data/timeline.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Jobe title',
+        duration: 'years',
+        details:
+        ';lkjadfs;lkjadfgl;kjadfg;lkjafg;lkadfg;lkhafg;lkafgd;ljkadfg;kljhadfg;kjhafdg',
+    },
+    {
+        year: 'Today',
+        title: 'Jobe title',
+        duration: 'years',
+        details:
+        ';lkjadfs;lkjadfgl;kjadfg;lkjafg;lkadfg;lkhafg;lkafgd;ljkadfg;kljhadfg;kjhafdg',
+    },
+    {
+        year: 'Today',
+        title: 'Jobe title',
+        duration: 'years',
+        details:
+        ';lkjadfs;lkjadfgl;kjadfg;lkjafg;lkadfg;lkhafg;lkafgd;ljkadfg;kljhadfg;kjhafdg',
+    },
+    {
+        year: 'Today',
+        title: 'Jobe title',
+        duration: 'years',
+        details:
+        ';lkjadfs;lkjadfgl;kjadfg;lkjafg;lkadfg;lkhafg;lkafgd;ljkadfg;kljhadfg;kjhafdg',
+    }
+]
